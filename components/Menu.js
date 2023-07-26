29853566a28993e65b745480fa9ee77ee24c39be
dykmalon <dykmalon@gmail.com>
2023-07-27 02:45:17 +0530

    box added

--- components/Menu.js
+++ components/Menu.js
@@ -8,18 +8,18 @@ export default function Menu() {
      <div className='menu-main'>
         <div className="menu">
 
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">LITECOIN</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">LITECOIN</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                            <a class="nav-link active" aria-current="page" href="#"><FaWallet />Wallet</a>
-                            <a class="nav-link" href="#"><FaSearch />Search</a>
-                            <a class="nav-link" href="#"><PiBracketsCurlyBold />Inscribe</a>
-                            <a class="nav-link" href="#">LTC-20</a>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <a className="nav-link active" aria-current="page" href="#"><FaWallet />Wallet</a>
+                            <a className="nav-link" href="#"><FaSearch />Search</a>
+                            <a className="nav-link" href="#"><PiBracketsCurlyBold />Inscribe</a>
+                            <a className="nav-link" href="#">LTC-20</a>
                         </div>
                     </div>
                 </div>
